9105eb0823f1e55d0d77473105b71e982e4f5f9d
Dmitriy Zorin <zorin_dima138@mail.ru>
2026-02-27 22:41:54 +0300

    fix bugs through testing

--- backend/app/routes/cart.py
+++ backend/app/routes/cart.py
@@ -24,11 +24,11 @@ class UpdateCartRequest(BaseModel):
 class RemoveCartRequest(BaseModel):
     cart: Dict[int,int] = {}
 
-@router.post("/add", response_model=CartResponse, status_code=status.HTTP_200_OK)
+@router.post("/add", status_code=status.HTTP_200_OK)
 def add_to_cart(request: AddToCartRequest, db: Session = Depends(get_db)):
     service = CartService(db)
     item = CartItemCreate(product_id=request.product_id, quantity=request.quantity)
-    updated_cart = service.add_to_cart(item, request.cart)
+    updated_cart = service.add_to_cart(request.cart, item)
     return {'cart': updated_cart}
 
 
@@ -37,14 +37,14 @@ def get_cart(cart_data: Dict[int,int], db: Session = Depends(get_db)):
     service = CartService(db)
     return service.get_cart_details(cart_data)
 
-@router.put("/update", response_model=CartResponse, status_code=status.HTTP_200_OK)
+@router.put("/update", status_code=status.HTTP_200_OK)
 def update_cart(request: UpdateCartRequest, db: Session = Depends(get_db)):
     service = CartService(db)
     item = CartItemUpdate(product_id=request.product_id, quantity=request.quantity)
     updated_cart = service.update_cart_item(request.cart, item)
     return {'cart': updated_cart}
 
-@router.delete("/remove/{product_id}", response_model=CartResponse, status_code=status.HTTP_200_OK)
+@router.delete("/remove/{product_id}", status_code=status.HTTP_200_OK)
 def remove_from_cart(product_id: int, request: RemoveCartRequest, db: Session = Depends(get_db)):
     service = CartService(db)
     updated_cart = service.remove_from_cart(request.cart, product_id)
